62ee4e520de6c66707a466a73250ba10d9678b59
chestahedron <kilian.nero@gmail.com>
2023-06-06 23:02:55 +1000

    Index

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,37 @@
 import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
 //import { greetUser } from '$utils/greet';
-gsap.to('.ball', {
+/*
+gsap.to('[is-ball="red"]', {
   x: '65dvw',
   duration: 2,
   delay: 1,
   stagger: 0.3,
   ease: 'power2.inOut',
-  margin: '3rem',
+  margin: '2rem',
   yoyo: true,
-  repeat: 1,
+  repeat: 0,
+});
+*/
+
+gsap.from('[is-ball="blue"]', {
+  x: '65dvw',
+  duration: 2,
+  delay: 1,
+  stagger: 0.3,
+  ease: 'power2.inOut',
+  margin: '2rem',
+  //yoyo: true,
+  //repeat: -1,
+  scrollTrigger: {
+    markers: true,
+    trigger: '[section="2"]',
+    scrub: true,
+    start: 'top 50%',
+    end: 'bottom 50%',
+  },
 });
 
 /*

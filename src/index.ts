5382cb02e2b97ba92226f266592ecbcc1da33fec
chestahedron <kilian.nero@gmail.com>
2023-06-06 16:40:36 +1000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import { gsap } from 'gsap';
 
 //import { greetUser } from '$utils/greet';
-gsap.to('.ball', { x: '35vw', duration: 1, delay: 0.5, stagger: 1 });
+gsap.to('.ball', {
+  x: '65dvw',
+  duration: 2,
+  delay: 1,
+  stagger: 0.3,
+  ease: 'power2.inOut',
+  margin: '3rem',
+  yoyo: true,
+  repeat: 1,
+});
 
 /*
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  //console.log('hi');
   gsap.to('.ball', { x: '35vw', duration: 1, delay: 0.5, stagger: 0.5 });
 });
 */

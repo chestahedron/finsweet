bed3209e0329db0de89761fde2cbb64c4ca5e11e
chestahedron <kilian.nero@gmail.com>
2023-06-06 16:02:57 +1000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { gsap } from 'gsap';
 
 //import { greetUser } from '$utils/greet';
+gsap.to('.ball', { x: '35vw', duration: 1, delay: 0.5, stagger: 1 });
 
+/*
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  console.log('hi');
-  gsap.from('.headline', { opacity: 0, duration: 5 });
+  //console.log('hi');
+  gsap.to('.ball', { x: '35vw', duration: 1, delay: 0.5, stagger: 0.5 });
 });
+*/

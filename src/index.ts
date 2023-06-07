19540989c4d8bbaf42a1b39894129dd569e610a9
chestahedron <kilian.nero@gmail.com>
2023-06-07 12:05:03 +1000

    build master

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
 import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
-import { greetUser } from '$utils/greet';
+//import { greetUser } from '$utils/greet';
 
+gsap.from('[is-ball="blue"]', {
+  x: '68dvw',
+  duration: 2,
+  delay: 1,
+  stagger: 0.3,
+  ease: 'power2.inOut',
+  margin: '2rem',
+  //yoyo: true,
+  //repeat: -1,
+  scrollTrigger: {
+    markers: false,
+    trigger: '[section="2"]',
+    scrub: true,
+    start: 'top 50%',
+    end: 'bottom 50%',
+  },
+});
+
+/*
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'Hansi Krüger';
-  greetUser(name);
+  gsap.to('.ball', { x: '35vw', duration: 1, delay: 0.5, stagger: 0.5 });
 });
+*/
